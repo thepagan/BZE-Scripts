fc57171ff37df72a9a290efd5ba0693c0ad617d4
Potato <potato@voidr.net>
2019-12-11 19:38:49 -0600

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Create Bootstrap
+
+## Prereq
+sudo apt-get install zip
+
+## Move to home dir
+cd ~
+
+## Check if bootstrap exists
+if [ ! -f bzedge_bootstrap_$(date +%d-%m-%Y).zip]
+    then
+        rm bzedge_bootstrap_$(date +%d-%m-%Y).zip
+fi
+
+if [ -d bootstrap ]
+    then
+        rm -rfv ./bootstrap
+fi
+
+## Recreate bootstrap dir
+mkdir bootstrap
+cd bootstrap
+
+cp -r ~/.bzedge/blocks .
+cp -r ~/.bzedge/chainstate .
+
+cd ~
+
+## Zip
+zip bzedge_bootstrap_$(date +%d-%m-%Y).zip bootstrap
+
+## Clean up
+rm -rfv ./bootstrap
